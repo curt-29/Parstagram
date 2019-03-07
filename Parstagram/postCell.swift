69ec3d2f7cb48bf5e7eb8e200084ba02626b582b
Curtis Lee <cdlee17@students.desu.edu>
2019-03-07 02:10:43 -0500

    Initial Commit

---
+++ Parstagram/postCell.swift
@@ -0,0 +1,24 @@
+//
+//  postCell.swift
+//  Parstagram
+//
+//  Created by Curtis Lee on 3/6/19.
+//  Copyright © 2019 clee. All rights reserved.
+//
+
+import UIKit
+
+class postCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
